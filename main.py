372992370caa96a5f762cbf90d1ea7da8fd09099
madvas <matus.lestan@gmail.com>
2023-04-14 19:55:34 +0200

    update code

--- main.py
+++ main.py
@@ -1,33 +1,68 @@
 import os
 import discord
 from discord.ext import commands
+import openai
 
-# Get the value of an environment variable
-playbotToken = os.environ.get('PLAYBOT')
-playbotClientID = os.environ.get('PLAYBOT_CLIENT_ID')
+# Get the value of environment variables
+playbot_token = os.environ.get('PLAYBOT')
+playbot_client_id = os.environ.get('PLAYBOT_CLIENT_ID')
+openai.api_key = os.environ.get('OPENAI_API_KEY')
 
 intents = discord.Intents.default()
 intents.messages = True
 intents.guilds = True
+intents.message_content = True
 
-client = commands.Bot(command_prefix='!', intents=intents)
+bot = commands.AutoShardedBot(command_prefix='/', intents=intents)
 
-
-@client.event
+@bot.event
 async def on_ready():
-  print('Bot is ready.')
+    print(f'Logged in as {bot.user.name}')
+    print('------')
 
+# Define a custom help command
+class CustomHelpCommand(commands.DefaultHelpCommand):
+    pass
 
-@client.event
+# Register the custom help command
+bot.help_command = CustomHelpCommand()
+
+@bot.event
 async def on_message(message):
-  if message.author == client.user:
-    return
+    if message.author == bot.user:
+        return
 
-  if message.content.startswith('hello'):
-    await message.channel.send('Hello there!')
+    if bot.user.mentioned_in(message):
+        # Check if bot is mentioned in the message
+        prompt = message.content.replace(f'<@!{bot.user.id}>', '').strip()
+        if prompt:
+            response = openai.Completion.create(
+                model="text-davinci-003",
+                prompt=prompt,
+                temperature=0.7,
+                max_tokens=100
+            )
+            generated_text = response.choices[0].text
+            await message.channel.send(generated_text)
 
-invite_url = discord.utils.oauth_url(
-  playbotClientID, permissions=discord.Permissions(permissions=534723950656))
-print(f'Invite URL: {invite_url}')
+    await bot.process_commands(message)
 
-client.run(playbotToken)
+@bot.event
+async def on_command_error(ctx, error):
+    if isinstance(error, commands.CommandNotFound):
+        # If command is not found, suggest similar commands
+        cmd = ctx.message.content.split()[0]
+        suggested_cmds = [cmd.name for cmd in bot.commands if cmd.name.startswith(cmd)]
+        if suggested_cmds:
+            suggested_cmds.sort()
+            await ctx.send(f"Command not found. Did you mean: `{', '.join(suggested_cmds)}`?")
+        else:
+            await ctx.send("Command not found. Please use a valid command.")
+    else:
+        raise error
+
+# invite_url = discord.utils.oauth_url(
+#  playbot_client_id, permissions=discord.Permissions(permissions=534723950656))
+# print(f'Invite URL: {invite_url}')
+
+bot.run(playbot_token)
